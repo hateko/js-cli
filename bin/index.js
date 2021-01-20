0bb83c5b60f9a7ceed6a734fb2c7917c9c52d7ce
mlh <menglinghao@talk-cloud.com>
2021-01-20 11:42:49 +0800

    update build api
    https://juejin.cn/post/6919308174151385096

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,10 @@ const program = require('commander');
 const updateChk = require('../lib/update');
 const setMirror = require('../lib/mirror');
 const dlTemplate = require('../lib/download');
+const cTemplate = require('../lib/create');
 const initProject = require('../lib/init');
+const { consoleColors } = require('../lib/utils');
+const { blue, green } = consoleColors;
 
 // 从 package.json 文件中请求 version 字段的值， -v 和 --version是参数
 
@@ -32,6 +35,29 @@ program
   })
 
 program
+  .command('create')
+  .description('create a project ')
+  .action(() => {
+    cTemplate()
+  })
+
+/* mycli start 运行项目 */
+program
+  .command('start')
+  .description('start a project')
+  .action(() => {
+    
+  })
+
+/* mycli build 打包项目 */
+program
+  .command('build')
+  .description('build a project')
+  .action(() => {
+    green('--------构建项目-------')
+  })
+
+program
   .name('js-plugin-cli')
   .usage('<commands>[options]')
   .command('init <project_name>')
@@ -40,5 +66,14 @@ program
     initProject(project)
   })
 
+program
+  .option('-d, --debug', 'output extra debugging')
+  .option('-s, --small', 'small pizza size')
+
 // 解析命令行参数
 program.parse(process.argv)
+if( program.debug ){
+  blue('option is debug')
+}else if(program.small){
+  blue('option is small')
+}
